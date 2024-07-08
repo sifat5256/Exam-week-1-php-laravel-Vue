066d4c122652fc993676a68730cab594bc6bc1e0
IQRAMUL HASAN SIFAT <iqramulhasan65@gmail.com>
2024-07-08 23:15:35 +0600

    first commit

---
+++ task2.php
@@ -0,0 +1,7 @@
+<?php
+$num1 = 55;
+$num2 = 75;
+$sum = $num1 + $num2;
+
+echo "The sum of $num1 and $num2 is: $sum";
+?>
---
+++ task3.php
@@ -0,0 +1,9 @@
+<?php
+$age = 30;
+
+if ($age >= 18 && $age < 65) {
+    echo "Eligible";
+} else {
+    echo "Not Eligible";
+}
+?>
